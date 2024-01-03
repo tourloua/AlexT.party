3f7c1d66073cab717be2af87119dc140dc2e9ef0
tourloua <144152554+tourloua@users.noreply.github.com>
2024-01-03 15:24:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const projectName = event.target.innerText;
             switch (projectName) {
                 case 'Google Data Analytics Capstone Project':
-                    window.location.href = 'https://notalextourloukis.github.io/alextourloukisdatacapstone/';
+                    window.location.href = 'https://tourloua.github.io/alextourloukisdatacapstone/';
                     break;
                 case 'ENGINEER 1P13 Final Project':
                     window.location.href = 'https://tourloua.github.io/1p13capstone/';
